966752e4b8fce0b41d9d23049b72eb5b01dda99b
schan2023 <lamschan996@ufl.edu>
2018-07-05 10:07:35 -0700

    Added UI elements and implemented logic

---
+++ TipCalculatorStarter/ColorTheme.swift
@@ -0,0 +1,27 @@
+//
+//  ColorTheme.swift
+//  TipCalculatorStarter
+//
+//  Created by Simone Chan on 7/5/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct ColorTheme {
+    
+    // MARK: - Instance Vars
+    
+    let isDefaultStatusBar: Bool
+    let viewControllerBackgroundColor: UIColor
+    
+    let primaryColor: UIColor
+    let primaryTextColor: UIColor
+    
+    let secondaryColor: UIColor
+    
+    let accentColor: UIColor
+    let outputTextColor: UIColor
+}
--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -14,6 +14,121 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
+        
+        billAmountTextField.calculateButtonAction = {
+            self.calculate()
+        }
+    }
+    
+//Header
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    //Actions
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("switch toggled on")
+        } else {
+            print("switch toggled off")
+        }
+    }
+    
+//Input Card
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: BillAmountTextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    
+    //Actions
+    @IBAction func tipPercentChanged(_ sender: UISegmentedControl) {
+        calculate()
+    }
+    
+//Output Card
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+
+//Reset Button
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+         clear()
+    }
+    
+//Helper functions
+    //Calculates tip
+    func calculate() {
+        if self.billAmountTextField.isFirstResponder {
+            self.billAmountTextField.resignFirstResponder()
+        }
+        
+        //Retrieves amount from text field
+        guard let billAmountText = self.billAmountTextField.text,
+            let billAmount = Double(billAmountText)
+            else {
+                clear()
+                return }
+        
+        //Rounds amount
+        let roundedBillAmount = (100 * billAmount).rounded() / 100
+        
+        //Calculates tip amount
+        let tipPercent: Double
+        switch tipPercentSegmentedControl.selectedSegmentIndex {
+        case 0:
+            tipPercent = 0.15
+        case 1:
+            tipPercent = 0.18
+        case 2:
+            tipPercent = 0.20
+        default:
+            preconditionFailure("Unexpected index.")
+        }
+        
+        let tipAmount = roundedBillAmount * tipPercent
+        let roundedTipAmount = (100 * tipAmount).rounded() / 100
+        
+        //Calculates total
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        // Update UI
+        self.billAmountTextField.text = String(format: "%.2f", roundedBillAmount)
+        self.tipAmountLabel.text = String(format: "%.2f", roundedTipAmount)
+        self.totalAmountLabel.text = String(format: "%.2f", totalAmount)
+    }
+    
+    //Resets to initial state
+    func clear() {
+        billAmountTextField.text = ""
+        tipPercentSegmentedControl.selectedSegmentIndex = 0
+        tipAmountLabel.text = "$0.00"
+        totalAmountLabel.text = "$0.00"
+    }
+    
+    //Sets up views
+    func setupViews() {
+        headerView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        headerView.layer.shadowOpacity = 0.05
+        headerView.layer.shadowColor = UIColor.black.cgColor
+        headerView.layer.shadowRadius = 35
+        
+        inputCardView.layer.cornerRadius = 8
+        inputCardView.layer.masksToBounds = true
+        
+        outputCardView.layer.cornerRadius = 8
+        outputCardView.layer.masksToBounds = true
+        
+        // set output card border
+        outputCardView.layer.borderWidth = 1
+        outputCardView.layer.borderColor = UIColor.tcHotPink.cgColor
+        
+        resetButton.layer.cornerRadius = 8
+        resetButton.layer.masksToBounds = true
     }
 }
 
